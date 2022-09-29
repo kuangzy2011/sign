da52704bb5b29d7db0e94b1db3aa08e8f8e7ac64
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2022-09-29 15:47:16 +0800

    Update bayesian_neural_network.py

--- bayesian_neural_network.py
+++ bayesian_neural_network.py
@@ -346,6 +346,7 @@ def main(argv):
                                           epoch, step)),
                                   title='mean heldout logprob {:.2f}'
                                   .format(heldout_log_prob))
+  model.summary()
   model.save('mnist_model')
   model.save_weights('bcnn/model_mnist_bcnn.model', overwrite=True, save_format='tf')
 
